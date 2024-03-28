4f5b9031166bb00aacbe1930044bb1754d65514f
Jacob-Mowat <zaxquit@protonmail.com>
2024-03-28 20:42:38 +0000

    [feat] Version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="unslab",
-    version="1.7.10",
+    version="1.8.10",
     packages=find_packages(
         include=["unslab", "unslab.*"],
     ),
