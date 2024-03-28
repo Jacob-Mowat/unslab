b165ad24368c00e2bd935996ee1a6372bd4382a3
Jacob-Mowat <zaxquit@protonmail.com>
2024-03-28 20:43:04 +0000

    [feat] Added PNGLoader

--- unslab/data/loader.py
+++ unslab/data/loader.py
@@ -42,11 +42,47 @@ class RAWLoader:
                 return
 
         # Post process the raw images
-        self.rgb_images = [img.postprocess(no_auto_bright=True,use_auto_wb=True,gamma=None) for img in self.raw_images]
+        self.rgb_images = [img.postprocess(no_auto_bright=False,use_auto_wb=False,gamma=None) for img in self.raw_images]
 
     def findRAWImagesInDir(self) -> None:
         # Find all the files in the directory -> assumes all files are images
         self.images = listdir(self.path)
+
+    def get_images(self) -> List[numpy.ndarray]:
+        return self.rgb_images
+
+class PNGLoader:
+    path: str
+    found_images: List[str]
+
+    rgb_images: List[numpy.ndarray] | None
+
+    def __init__(self, path):
+        self.path = path
+        self.images = None
+        self.rgb_images = None
+
+        self.load_png_images()
+
+    def load_png_images(self) -> None:
+        # If images are not found, find them
+        if self.images is None:
+            self.findPNGImagesInDir()
+
+            if self.images is None:
+                print("No images found in directory")
+                return
+
+        # Create a list of paths to the images
+        paths: List[str] = [self.path + img for img in self.images]
+
+        # Load the raw images
+        self.rgb_images = [imageio.imread(path) for path in paths]
+
+    def findPNGImagesInDir(self) -> None:
+        # Find all the files in the directory -> assumes all files are images
+        files = listdir(self.path)
+        self.images = [file for file in files if file.endswith(".png")]
 
     def get_images(self) -> List[numpy.ndarray]:
         return self.rgb_images
